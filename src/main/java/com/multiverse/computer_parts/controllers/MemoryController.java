a0c9be8dca8f091c5546865fe9eb2cc1a14b091a
multiverse-95 <vyvalkov@yandex.ru>
2022-07-03 21:23:13 +0700

    #1 Added api memory by guid.

--- src/main/java/com/multiverse/computer_parts/controllers/MemoryController.java
+++ src/main/java/com/multiverse/computer_parts/controllers/MemoryController.java
@@ -5,14 +5,22 @@
 
 package com.multiverse.computer_parts.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.multiverse.computer_parts.dto.memory.MemoryDto;
+import com.multiverse.computer_parts.dto.processors.ProcessorsDto;
 import com.multiverse.computer_parts.enums.ComputerParts;
 import com.multiverse.computer_parts.service.SerializationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/memory")
@@ -27,5 +35,23 @@ public class MemoryController {
         String jsonMemory = computerPartsEnums.json();
         memoryDtoList = serializationService.serializeMemory(jsonMemory);
         return ResponseEntity.ok(memoryDtoList);
+    }
+
+
+    @GetMapping("/product/{guid}")
+    public ResponseEntity<Object> getVideoCardById(@PathVariable String guid) throws JsonProcessingException {
+        MemoryDto memoryDto = new MemoryDto();
+        ComputerParts computerPartsEnums = ComputerParts.MEMORY;
+        String jsonMemory = computerPartsEnums.json();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<MemoryDto> myObjects = objectMapper.readValue(jsonMemory, new TypeReference<List<MemoryDto>>(){});
+        for (MemoryDto myObject : myObjects) {
+            if (myObject.getGuid().equals(guid)) {
+                memoryDto = myObject;
+                return ResponseEntity.ok(memoryDto);
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
