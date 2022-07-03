a5d7d1e360181a317c4e2c08b23269df910521fc
multiverse-95 <vyvalkov@yandex.ru>
2022-07-03 15:52:01 +0700

    Added videoCard dto.

---
+++ src/main/java/com/multiverse/computer_parts/dto/videoCards/DetailsVideoCardDto.java
@@ -0,0 +1,52 @@
+package com.multiverse.computer_parts.dto.videoCards;
+
+public class DetailsVideoCardDto {
+    private String fullName;
+    private String manufacturer;
+    private String videoMemorySize;
+    private String busBitWidth;
+
+    public DetailsVideoCardDto() {
+    }
+
+    public DetailsVideoCardDto(String fullName, String manufacturer, String videoMemorySize, String busBitWidth) {
+        this.fullName = fullName;
+        this.manufacturer = manufacturer;
+        this.videoMemorySize = videoMemorySize;
+        this.busBitWidth = busBitWidth;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getVideoMemorySize() {
+        return videoMemorySize;
+    }
+
+    public void setVideoMemorySize(String videoMemorySize) {
+        this.videoMemorySize = videoMemorySize;
+    }
+
+    public String getBusBitWidth() {
+        return busBitWidth;
+    }
+
+    public void setBusBitWidth(String busBitWidth) {
+        this.busBitWidth = busBitWidth;
+    }
+}
+
+
