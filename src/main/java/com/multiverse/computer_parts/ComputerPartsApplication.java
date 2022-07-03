3b970d140a2fd22284e94b9cb59cdd9222a65dca
multiverse-95 <vyvalkov@yandex.ru>
2022-07-03 15:11:27 +0700

    Initial commit

---
+++ src/main/java/com/multiverse/computer_parts/ComputerPartsApplication.java
@@ -0,0 +1,13 @@
+package com.multiverse.computer_parts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerPartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerPartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multiverse/computer_parts/ComputerPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiverse.computer_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerPartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
